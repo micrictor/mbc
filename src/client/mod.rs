7ce2ca9f89c44bd0012069e13fc60fb40fdd654b
Michael Torres <mic.ric.tor@gmail.com>
2022-10-09 00:07:34 +0000

    Add read_device_identification methods

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
 use tokio_serial;
@@ -11,6 +12,8 @@ use crate::uri::Proto;
 
 const READ_FILE_RECORD: u8 = 0x14;
 const READ_FIFO_QUEUE: u8 = 0x18;
+const READ_DEVICE_IDENTIFICATION: u8 = 0x2B;
+const MEI_CODE: u8 = 0x0E;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FileRecord {
@@ -40,6 +43,111 @@ impl TryFrom<Vec<u8>> for FileRecord {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[repr(u8)] 
+pub enum DeviceIdentificationCode {
+    Basic = 1,
+    Regular = 2,
+    Extended = 3,
+    Individual = 4,
+    Unknown = 0xFF,
+}
+
+impl From<u8> for DeviceIdentificationCode {
+    fn from(v: u8) -> Self {
+        match v {
+            1 => DeviceIdentificationCode::Basic,
+            2 => DeviceIdentificationCode::Extended,
+            3 => DeviceIdentificationCode::Regular,
+            4 => DeviceIdentificationCode::Individual,
+            _ => DeviceIdentificationCode::Unknown,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum DeviceConformity {
+    Basic = 1,
+    Regular = 2,
+    Extended = 3,
+    BasicWithIndividual = 0x81,
+    RegularWithIndividual = 0x82,
+    ExtendedWithIndividual = 0x83,
+    Unknown,
+}
+
+impl From<u8> for DeviceConformity {
+    fn from(v: u8) -> Self {
+        match v {
+            1 => DeviceConformity::Basic,
+            2 => DeviceConformity::Extended,
+            3 => DeviceConformity::Regular,
+            0x81 => DeviceConformity::BasicWithIndividual,
+            0x82 => DeviceConformity::ExtendedWithIndividual,
+            0x83 => DeviceConformity::RegularWithIndividual,
+            _ => DeviceConformity::Unknown,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DeviceIDObject {
+    pub id: u8,
+    pub length: u8,
+    pub value: Vec<u8>,
+}
+
+impl From<VecDeque<u8>> for DeviceIDObject {
+    fn from(mut v: VecDeque<u8>) -> Self {
+        let id = v.pop_front()
+            .unwrap();
+        let length = v.pop_front()
+            .unwrap();
+        DeviceIDObject { id, length, value: v.clone().into() }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DeviceIdentification {
+    pub mei_type: u8,
+    pub read_device_id_code: DeviceIdentificationCode,
+    pub conformity_level: DeviceConformity,
+    pub more_follows: u8,
+    pub next_object_id: u8,
+    pub number_of_objects: u8,
+    pub objects: Vec<DeviceIDObject>,
+}
+
+impl From<VecDeque<u8>> for DeviceIdentification {
+    fn from(mut data: VecDeque<u8>) -> DeviceIdentification {
+        let mei_type = data.pop_front()
+            .unwrap();
+        let read_device_id_code: DeviceIdentificationCode = data.pop_front()
+            .unwrap()
+            .into();
+        let conformity_level: DeviceConformity = data.pop_front()
+            .unwrap()
+            .into();
+        let more_follows = data.pop_front()
+            .unwrap();
+        let next_object_id = data.pop_front()
+            .unwrap();
+        let number_of_objects = data.pop_front()
+            .unwrap();
+
+        let mut objects: Vec<DeviceIDObject> = vec![];
+        for _ in 0..number_of_objects {
+            let object_len = data[1];
+            let buffer_length: usize = usize::from(2 + object_len);
+            let mut object_buffer = data.clone();
+            object_buffer.truncate(buffer_length);
+            objects.push(object_buffer.into());
+        }
+
+        DeviceIdentification{mei_type, read_device_id_code, conformity_level, more_follows, next_object_id, number_of_objects, objects}
+    }
+}
+
 #[async_trait]
 pub trait ReaderExt: Reader {
     /// Read a file record.
@@ -49,6 +157,8 @@ pub trait ReaderExt: Reader {
     /// Read a First-In, First-Out (FIFO) queue of registers on the remote device.
     /// Up to 31 data registers can be read. Queue contents are read, but not cleared.
     async fn read_fifo_queue(&mut self, pointer_address: u16) -> Result<Vec<u16>, Error>;
+    /// Read device identification information. Requires device to implement Modbus Encapsulated Interface (MEI).
+    async fn read_device_identification(&mut self, id_code: DeviceIdentificationCode, object_id: u8) -> Result<DeviceIdentification, Error>;
 }
 
 #[async_trait]
@@ -84,6 +194,19 @@ impl ReaderExt for Context {
                 }
 
                 Ok(response_vec)
+            },
+            _ => Err(Error::new(ErrorKind::InvalidData, "invalid response data"))
+        }
+    }
+
+    async fn read_device_identification(&mut self, id_code: DeviceIdentificationCode, object_id: u8) -> Result<DeviceIdentification, Error> {
+        let request: Vec<u8> = vec![MEI_CODE, id_code as u8, object_id];
+        
+        let rsp = self.call(Request::Custom(READ_DEVICE_IDENTIFICATION, request)).await?;
+        match rsp {
+            Response::Custom(_func_code, raw_vec) => {
+                let vecdeq: VecDeque<u8> = raw_vec.into();
+                Ok(vecdeq.into())
             },
             _ => Err(Error::new(ErrorKind::InvalidData, "invalid response data"))
         }
