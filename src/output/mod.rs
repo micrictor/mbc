5a1f3dce907fc8a8cd6c9319268c253005c86015
Michael Torres <mic.ric.tor@gmail.com>
2022-10-15 18:02:50 +0000

    Reduce import length

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,6 +1,7 @@
 use anyhow::Error;
 
-pub mod tsv;
+mod tsv;
+pub use crate::output::tsv::TsvOutput;
 
 pub trait Output {
     fn write_output(&mut self, columns: Vec<String>, rows: Vec<Vec<String>>) -> Result<(), Error>;
